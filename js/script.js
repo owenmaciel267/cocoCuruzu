5682b59b4f813b1d1cc44ff346b6808fd585fe03
owenmaciel267 <owentiburon32@gmail.com>
2023-11-01 11:48:55 -0300

    haciendo la pagina productos

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// const miH1 = document.getElementById('coco');
+
+// for (let i = 0; i < miH1.textContent.length; i++) {
+//   if (miH1.textContent[i] === "C") {
+//     miH1.innerHTML = miH1.innerHTML.substring(0, i) + `<span style="color: green;">${miH1.textContent[i]}</span>` + miH1.innerHTML.substring(i + 1);
+//   }
+// }
+
+
+
+
+
+var swiperr = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+   
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+          slidesPerView: 1
+        },
+        520: {
+          slidesPerView: 2
+        },
+        950: {
+          slidesPerView: 3
+        },
+      },
+});
+
